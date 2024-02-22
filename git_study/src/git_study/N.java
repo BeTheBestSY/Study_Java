23dca1ff83e1e1c37581ed647e15853a245cb44e
SY <h6118@naver.com>
2024-02-22 21:30:29 +0900

    21:30

--- git_study/src/git_study/N.java
+++ git_study/src/git_study/N.java
@@ -1,11 +1,15 @@
 package git_study;
 
+import java.util.Scanner;
+
 public class N {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// 예시입니다.
 		// 추가합니다.
+		
+		Scanner sc = new Scanner(System.in);
 	}
 
 }
